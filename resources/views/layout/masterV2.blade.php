b3509c3c013fd989a364e59075a1aad36abc18cb
wassim-kaouia <wassim.elkaouia@gmail.com>
2022-10-20 17:31:09 +0100

    ok

--- resources/views/layout/masterV2.blade.php
+++ resources/views/layout/masterV2.blade.php
@@ -125,7 +125,7 @@
                             <div class="dropdown-menu dropdown-menu-end">
                                 <!-- item-->
                                 <a class="dropdown-item" href="#"><i class="bx bx-user font-size-16 align-middle me-1"></i> <span key="t-profile">Profile</span></a>
-                                <a class="dropdown-item d-block" href="#"><i class="bx bx-wrench font-size-16 align-middle me-1"></i> <span key="t-settings">Settings</span></a>
+                                <a class="dropdown-item d-block" href="#"> <i class="bx bx-wrench font-size-16 align-middle me-1"></i> <span key="t-settings">Settings</span></a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item text-danger" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
