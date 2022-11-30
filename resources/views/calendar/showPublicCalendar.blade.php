9c833c0c8228d708654a71a2040491b5a8f3e9c6
wassim-kaouia <wassim.elkaouia@gmail.com>
2022-11-30 09:42:07 +0000

    disable december holidays

--- resources/views/calendar/showPublicCalendar.blade.php
+++ resources/views/calendar/showPublicCalendar.blade.php
@@ -258,6 +258,9 @@
             },
         });
         $(".datepicker").flatpickr({
+            disable: ['2022-12-24','2022-12-25','2022-12-26','2022-12-27','2022-12-28','2022-12-29','2022-12-30','2022-12-31','2023-01-01'], 
+            //set past dates disabled
+            minDate: "today",
             locale:{
                 firstDayOfWeek: 1,
                 weekdays: {
