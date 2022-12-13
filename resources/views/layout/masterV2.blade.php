a502de0d4981ccda9ef21c43b0d992a388294224
wassim-kaouia <wassim.elkaouia@gmail.com>
2022-12-13 08:56:36 +0000

    title

--- resources/views/layout/masterV2.blade.php
+++ resources/views/layout/masterV2.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>@yield('title')</title>
+        <title>@yield('mytitle')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Calendar Management Systeme For OVM By Wassim Kaouia" name="description" />
         <meta content="Wassim Kaouia" name="author" />
